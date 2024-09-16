e8a6d6384feeaffd355c286f5c91fdde73a73f3a
Dave Mollen <davemollen@gmail.com>
2024-09-16 09:58:49 +0200

    Switch to zip archive instead of tar

--- src-tauri/src/services/zip_service.rs
+++ src-tauri/src/services/zip_service.rs
@@ -2,33 +2,41 @@ use crate::mod_plugin_controller::ArrayBufferWithPath;
 use std::fs::{self, File};
 use std::io::{self, Read};
 use std::path::PathBuf;
-use tar::Archive;
+use zip::ZipArchive;
 
 pub struct ZipService;
 
 impl ZipService {
     pub fn unzip(file_path: &PathBuf) -> Result<(), io::Error> {
         let file = File::open(file_path)?;
-        let mut archive = Archive::new(file);
+        let mut archive = ZipArchive::new(file)?;
 
-        for entry in archive.entries()? {
-            let mut entry = entry?;
-            let path = entry.path()?;
-            let out_path = file_path.parent().unwrap().join(PathBuf::from(path));
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+            let path = match file.enclosed_name() {
+                Some(path) => path,
+                None => continue,
+            };
+            let folder = match file_path.parent() {
+                Some(path) => path,
+                None => continue,
+            };
+            let output_path = folder.join(PathBuf::from(path));
 
             // If the entry is a directory, create it
-            if entry.header().entry_type().is_dir() {
-                fs::create_dir_all(&out_path)?;
+            if file.is_dir() {
+                fs::create_dir_all(&output_path)?;
             } else {
                 // Create parent directories if they don't exist
-                if let Some(p) = out_path.parent() {
+                if let Some(p) = output_path.parent() {
                     if !p.exists() {
                         fs::create_dir_all(&p)?;
                     }
                 }
 
                 // Extract the file
-                entry.unpack(&out_path)?;
+                let mut output_file = File::create(&output_path)?;
+                io::copy(&mut file, &mut output_file)?;
             }
         }
 
@@ -41,22 +49,30 @@ impl ZipService {
     ) -> Result<Vec<ArrayBufferWithPath>, io::Error> {
         let mut result: Vec<ArrayBufferWithPath> = Vec::new();
         let file = File::open(file_path)?;
-        let mut archive = Archive::new(file);
+        let mut archive = ZipArchive::new(file)?;
 
-        for entry in archive.entries()? {
-            let mut entry = entry?;
-            let path = entry.path()?;
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+            let path = match file.enclosed_name() {
+                Some(path) => path,
+                None => continue,
+            };
             if !path.starts_with(starts_with) {
                 continue;
             };
-
-            let stripped_path = path.strip_prefix(starts_with.parent().unwrap()).unwrap();
+            let starts_with_parent = match starts_with.parent() {
+                Some(parent) => parent,
+                None => continue,
+            };
+            let stripped_path = match path.strip_prefix(starts_with_parent) {
+                Ok(p) => p,
+                Err(_) => continue,
+            };
             let path = stripped_path.to_string_lossy().to_string();
 
-            if entry.header().entry_type().is_file() {
+            if file.is_file() {
                 let mut buffer = Vec::new();
-                entry.read_to_end(&mut buffer)?;
-
+                file.read_to_end(&mut buffer)?;
                 result.push(ArrayBufferWithPath { path, buffer })
             }
         }
